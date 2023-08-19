cf264625b602961c51f49dd0f58581429e47887b
Gabriel Augusto <gafb.xpenergy@gmail.com>
2023-08-19 13:48:23 -0300

    feat: theme basics and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
-import './App.css'
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
 
   return (
+    <ThemeProvider theme={defaultTheme}>
     <h1> Inited project</h1>
+    <GlobalStyle/>
+    </ThemeProvider>
   )
 }
 
